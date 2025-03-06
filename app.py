69b4d21468085927c8a3e5fca0eba49229e5360d
Maliha Baig <161885144+malihabaig@users.noreply.github.com>
2025-03-06 14:33:44 +0500

    app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ if uploaded_files:
 
         if file_ext == ".csv":
            df = pd.read_csv(file)
-        elif file_ext == ".xlsx":
-            df = pd.read_excel(file, engine='openpyxl')
+        elif file_ext == "xlsx":
+            df = pd.read_excel(file)
         else:
             st.error(f"unsupported file type: {file_ext}")
             continue
